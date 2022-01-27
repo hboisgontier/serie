76ecb01cb271a33435da766c84eb21ef1b1f6e77
Herve BOISGONTIER <hboisgontier@eni-ecole.fr>
2022-01-27 09:43:01 +0100

    doctrine

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,23 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SerieController extends AbstractController
 {
     #[Route('/serie', name: 'serie_list')]
-    public function index(): Response
+    public function index(SerieRepository $repo): Response
     {
-        return $this->render('serie/list.html.twig');
+        $series = $repo->findAll();
+        //$series =$repo->findBy([], ['vote'=>'DESC']);
+        return $this->render('serie/list.html.twig', ['series'=>$series]);
     }
 
     #[Route('/serie/{id}', name: 'serie_detail', requirements:['id' => '\d+'], methods:['GET'])]
-    public function detail($id) : Response {
-        dump($id);
+    public function detail($id, SerieRepository $repo) : Response {
+        $serie = $repo->find($id);
+        if(!$serie)
+            throw new NotFoundHttpException();
         return $this->render('serie/detail.html.twig',
-            compact("id"));
+            compact("id", "serie"));
+    }
+
+    #[Route('/serie/add', name: 'serie_add')]
+    public function add(EntityManagerInterface $em): Response {
+        $serie = new Serie();
+        $serie->setName('test serie')
+                ->setTmdbId(854)
+                ->setDateCreated(new \DateTime());
+        $em->persist($serie);
+        $em->flush();
+        $serie->setOverview('bla bla...');
+        $serie->setDateModified(new \DateTime());
+        $em->flush();
+        $em->remove($serie);
+        $em->flush();
+        return $this->render('serie/add.html.twig');
     }
 }
